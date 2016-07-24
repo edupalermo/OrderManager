b62480fc44d79495b6ccb2f25cdbb3c77a9e378f
unknown <eduardo.palermo@m4u.com.br>
2016-07-24 15:55:37 -0300

    Lock draft

--- src/main/java/org/palermo/repository/OrderRepository.java
+++ src/main/java/org/palermo/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package org.palermo.repository;
 
+import javax.persistence.LockModeType;
+
 import org.palermo.entity.Order;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_READ)
     Order findByNumber(String number);
     
 }
--- src/main/java/org/palermo/service/OrderService.java
+++ src/main/java/org/palermo/service/OrderService.java
@@ -1,24 +1,43 @@
 package org.palermo.service;
 
+import java.util.UUID;
+
 import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
 
 import org.palermo.entity.Item;
 import org.palermo.entity.Order;
+import org.palermo.exception.DuplicatedEntityException;
 import org.palermo.repository.ItemRepository;
 import org.palermo.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
+@Validated
 public class OrderService {
     
     @Autowired OrderRepository orderRepository;
     
     @Autowired ItemRepository itemRepository;
-    
+
     @Transactional
-    public void save(Order order) {
-        orderRepository.save(order);
+    public Order save(@NotNull(message="A null order cannot be saved") Order order) throws DuplicatedEntityException {
+    	
+    	// The specification says that the number is aa optional field. But if it is null, we will generate a random value
+    	if (order.getNumber() == null) {
+    		order.setNumber(UUID.randomUUID().toString());
+    	}
+    	else {
+        	Order persistedOrder = orderRepository.findByNumber(order.getNumber());
+        	
+        	if (persistedOrder != null) {
+        		throw new DuplicatedEntityException(String.format("Order with number[%s] aleady existis!", order.getNumber()));
+        	}
+    	}
+    	
+        return orderRepository.save(order);
     }
 
     public Order findByNumber(String number) {
